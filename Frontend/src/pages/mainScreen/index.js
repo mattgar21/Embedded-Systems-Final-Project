cae85b42a3de2982e7428a811b98f5c025d45776
Matthew <matthew.j.garcia00@gmail.com>
2025-11-20 20:42:35 -0600

    Updated with port forward

--- Frontend/src/pages/mainScreen/index.js
+++ Frontend/src/pages/mainScreen/index.js
@@ -6,7 +6,61 @@ import { styled } from "@mui/material/styles";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 
-// ... MaterialUISwitch (unchanged) ...
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          "#fff"
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: "#aab4be",
+        ...theme.applyStyles("dark", {
+          backgroundColor: "#8796A5",
+        }),
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: "#001e3c",
+    width: 32,
+    height: 32,
+    "&::before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        "#fff"
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#003892",
+    }),
+  },
+  "& .MuiSwitch-track": {
+    opacity: 1,
+    backgroundColor: "#aab4be",
+    borderRadius: 20 / 2,
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#8796A5",
+    }),
+  },
+}));
 
 export default function MainScreen() {
   return (
@@ -20,19 +74,20 @@ export default function MainScreen() {
         p: 4,
       }}
     >
+      {/* Main horizontal layout: graphs on left, switches on right */}
       <Box
         sx={{
           display: "flex",
           flexDirection: "row",
-          alignItems: "center",
           justifyContent: "space-between",
+          width: "100%",
         }}
       >
-        {/* small window for graphs */}
+        {/* Left side: Graphs */}
         <Box
           sx={{
             width: 800, // natural (unscaled) layout width
-            transform: "scale(1.2)", // shrink everything
+            transform: "scale(1.2)",
             transformOrigin: "top left",
           }}
         >
@@ -55,7 +110,7 @@ export default function MainScreen() {
               }}
             >
               <Typography variant="h6" gutterBottom>
-                Sensor 1
+                USB C port 1
               </Typography>
               <LineChart
                 xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
@@ -76,7 +131,7 @@ export default function MainScreen() {
               }}
             >
               <Typography variant="h6" gutterBottom>
-                Sensor 2
+                USB C port 2
               </Typography>
               <LineChart
                 xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
@@ -113,24 +168,26 @@ export default function MainScreen() {
           </Paper>
         </Box>
 
-        <Paper>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <FormControlLabel
-              control={<Switch defaultChecked />}
-              label="MUI switch"
-            />
-
-            <FormControlLabel
-              control={<Switch defaultChecked />}
-              label="MUI switch"
-            />
-          </Box>
+        {/* Right side: Switch panel */}
+        <Paper
+          sx={{
+            p: 3,
+            ml: 4,
+            minWidth: 250,
+            height: "fit-content",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <FormControlLabel
+            control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+            label="USB C port 1"
+          />
+          <FormControlLabel
+            control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+            label="USB C port 2"
+          />
         </Paper>
       </Box>
     </Box>
